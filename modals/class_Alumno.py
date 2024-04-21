411c000b08b746fba6afb6c04b952faa0fee6958
Angel G300 Saucedo <saucedog926@gmail.com>
2024-04-21 20:23:59 -0300

    implementando la funcionalidad de calcular materias desbloqueadas, y calculamos su importancia.

--- modals/class_Alumno.py
+++ modals/class_Alumno.py
@@ -15,21 +15,25 @@ class GestionAlumno(Alumno):
         super().__init__(nombre, cod_materias_aprob, carrera)
 
         self.materias_aprob = []
+        self.materias_desbloqueadas = []
         self.cod_list_correlativas = []
 
-        ## iteramos en todas las materias.get_codigo() y vemos si estan dentro de la lista de aprobadas.
-        ## tambien, aprovechamos las iteraciones y agregamos la lista de correlativas de todas las no aprobadas.
-
+        ## iteramos todas las materias de la carrera y, buscamos las instancias de materias aprobadas,
+        ## si no esta aprobada se prosigue a ver si es una materia desbloqueada.
         for materia in Import(super().get_carrera()).get_load():
             agregar_materia = (materia.get_codigo() in self.cod_materias_aprob) and (len(self.cod_materias_aprob) > len(self.materias_aprob))    
             if agregar_materia:
                 self.materias_aprob.append(materia)
                 continue
+            m_desbloqueada = set(cod_materias_aprob).issuperset(materia.get_correlativas())
+            if m_desbloqueada or (len(materia.get_correlativas()) == 0): self.materias_desbloqueadas.append(materia)
             self.cod_list_correlativas += materia.get_correlativas()
         
-        ## setteamos la importancia de todas las materias aprobadas.
-        for importancia in self.materias_aprob:
+        ## setteamos la importancia de todas las materias aprobadas y materias desbloqueadas.
+        calc_importancia = self.materias_aprob+self.materias_desbloqueadas
+        for importancia in calc_importancia:
             importancia.set_importancia(self.cod_list_correlativas.count(importancia.get_codigo()))
 
     ##getters
     def get_materias_aprob(self): return self.materias_aprob
+    def get_materias_desbloqueadas(self): return self.materias_desbloqueadas
--- modals/main.py
+++ modals/main.py
@@ -1,19 +1,11 @@
-# from Bexport_Bimport import Import
-# from class_Materias import Materia
 from class_Alumno import GestionAlumno
 
-# materia = Import("LGTI").get_load()
+## Con la instancia "alumno" gestionamos y calculamos la importancia de las materias aprobadas, y desbloqueadas.
+alumno = GestionAlumno("Angel",[],"LGTI")
 
-hola = GestionAlumno("Angel",[6001,6002,6004,6013],"LGTI")
+for i in alumno.get_materias_aprob():
+    print(i)
 
-for i in hola.get_materias_aprob():
-    print(i.get_importancia())
-
-## materia aprobada => lista de codigo de materias que el alumno ingresa
-
-## calcular la importancia de todas las materias (depende de las materias aprobadas del alumno)
-## => cantidad de veces que aparece en el [conjunto listas de correlativas].count() de materias no aprobadas
-
-## materias desbloqueadas => una materia estara desbloqueada si todas las correlativas estan aprobadas
-
-## buscar interseccion entre dos sets de materias desbloqueadas
+print("*"*10)
+for i in alumno.get_materias_desbloqueadas():
+    print(i)
