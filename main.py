15526006adb168c8f2b65fc6842c0b89e0449181
Angel G300 Saucedo <saucedog926@gmail.com>
2024-05-09 22:22:14 -0300

    fix duplicates in 'interseccion_materias_desbloqueadas'

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from class_Alumno import GestionAlumno
 
 ## Con la instancia "alumno" gestionamos y calculamos la importancia de las materias aprobadas, y desbloqueadas.
-alumno1 = GestionAlumno("Angel",[6001,6002,6003,6005,6006,6007,6038,6041,6043],"LGTI")
-alumno2 = GestionAlumno("Nelson",[6001,6002,6003,6004,6010,6011],"LGTI")
+alumno1 = GestionAlumno("Angel",[6001,6002,6003,6004,6005,6006,6007,6008,6019],"LGTI")
+aaron = GestionAlumno("aaron",[6001,6002,6003,6004,6005,6006,6007,6008,6010,6019,6011],"LGTI")
+
+# for i in alumno1.get_materias_aprob(): print(i)
+
+paulina = GestionAlumno("Paulina",[6001],"LGTI")
 
 ## metodo para ver las materias que podrian cursar la instancia alumno1 con una lista de otros alumnos
-for i in alumno1.interseccion_materias_desbloqueadas([alumno2]): print(i)
+for i in alumno1.interseccion_materias_desbloqueadas([aaron,paulina]): print(i)
